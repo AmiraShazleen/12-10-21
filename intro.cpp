59e84bc39ff6e1b1576c0abc10409839977e3ffd
AmiraShazleen <shazleen.amin@gmail.com>
2021-10-18 15:37:44 +0800

    Create intro.cpp

---
+++ intro.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;      //namespace: to use cin, cout, and endl etc from the header file included.     
+int main()
+{
+	//display output
+	cout << "Intro to code";
+	
+	//code run as expected and will return the value 0
+	
+	return 0;
+	
+}
+
+
